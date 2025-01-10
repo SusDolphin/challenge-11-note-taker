62b1c066e398aa553005c18cf3bec5855f1b5764
Larry Fletcher <susdolphin@Larrys-Laptop.local>
2025-01-10 18:54:53 -0500

    api routes and index.js for html folder

---
+++ routes/html/index.js
@@ -0,0 +1,8 @@
+const router=require("express").Router()
+const path=require("path");
+
+router.get("/notes",(req,res)=>{})
+
+router.get("*", (req,res)=>{})
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const route = require("./routes/index.js")
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:"+PORT);
+});
